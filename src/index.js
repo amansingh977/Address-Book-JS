83c65edfc9522704c9d07969b16c39d61e3638b3
amansingh977 <amansinghrajawat09@gmail.com>
2025-03-03 16:15:41 +0530

    Aman Add. Added ability to find and delete contact by name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Aman", "Singh", "Bhopal", "India", "In", "100001", "9876543210", "amansinghrajawat@gmail.com"
+        "Aman", "Singh", "Bhopal", "India", "In", "100001", "9876543210", "amansinghrajawat09@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
@@ -16,6 +16,8 @@ try {
     console.log("All Contacts Before editing", addressBook.getAllContacts());
     console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Rakesh"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
